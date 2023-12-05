1b919aa467260aec718532a10852aea6466fc0a6
Łukasz Leśniak <lukaszlesniaksw@gmail.com>
2023-12-05 16:10:06 +0100

    Change searchbar to func-based, used hooks

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,30 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import css from './Searchbar.module.css';
-import PropTypes from 'prop-types';
+import { useFinder } from 'hooks/useFinder';
 
-export default class Searchbar extends Component {
-  render() {
-    return (
-      <header className={css.Searchbar}>
-        <form className={css.SearchForm} onSubmit={this.props.onSearch}>
-          <button type="submit" className={css.SearchFormButton}>
-            <span className={css.SearchFormLabel}>Search</span>
-          </button>
+export default function Searchbar() {
+  const { getSearch, getData, addImages, setPage } = useFinder();
 
-          <input
-            className={css.SearchFormInput}
-            name="search"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const search = getSearch(e.currentTarget);
+    const imagesFound = await getData(search, 1);
+    setPage(1);
+    addImages(imagesFound, false);
+  };
+
+  return (
+    <header className={css.Searchbar}>
+      <form className={css.SearchForm} onSubmit={handleSubmit}>
+        <button type="submit" className={css.SearchFormButton}>
+          <span className={css.SearchFormLabel}>Search</span>
+        </button>
+
+        <input
+          className={css.SearchFormInput}
+          name="search"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
 }
 
-Searchbar.propTypes = {
-  onSearch: PropTypes.func.isRequired,
-};
+// Searchbar.propTypes = {
+//   onSearch: PropTypes.func.isRequired,
+// };
