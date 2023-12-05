cf1f27f3882cdcf9746a8bdeb99e0228cd1c5971
Łukasz Leśniak <lukaszlesniaksw@gmail.com>
2023-12-05 16:10:33 +0100

    Change imagegallery to func-based, used hooks

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,27 +1,16 @@
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
-import React, { Component } from 'react';
+import React from 'react';
 import css from './ImageGallery.module.css';
-import PropTypes from 'prop-types';
+import { useFinder } from 'hooks/useFinder';
 
-export default class ImageGallery extends Component {
-  render() {
-    return (
-      <ul className={css.ImageGallery}>
-        {this.props.images.map(image => (
-          <ImageGalleryItem
-            key={image.id}
-            data={image}
-            onModal={this.props.onModal}
-            isModalOpen={this.props.isModalOpen}
-          />
-        ))}
-      </ul>
-    );
-  }
+export default function ImageGallery() {
+  const { images } = useFinder();
+
+  return (
+    <ul className={css.ImageGallery}>
+      {images.map(image => (
+        <ImageGalleryItem key={image.id} data={image} />
+      ))}
+    </ul>
+  );
 }
-
-ImageGallery.propTypes = {
-  images: PropTypes.array.isRequired,
-  onModal: PropTypes.func.isRequired,
-  isModalOpen: PropTypes.bool.isRequired,
-};
